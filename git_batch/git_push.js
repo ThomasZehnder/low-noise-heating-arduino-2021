e127f61085a26514b0ffa46dac5b28166954c11e
Thomas Zehnder <fly@zhs.ch>
2020-10-18 19:02:45 +0200

    test removed

--- git_batch/git_push.js
+++ git_batch/git_push.js
@@ -1,4 +1,4 @@
 
 var shell = require("shelljs");
 
-shell.exec("git push --set-upstream https://github.com/ThomasZehnder/low-noise-heating-arduino-2021.git master");
+shell.exec("git push --set-upstream https://github.com/ThomasZehnder/low-noise-heating-arduino-2021.git");
