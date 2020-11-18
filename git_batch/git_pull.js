4702a9416b6e896d659d7f8b2b2d17a8064c8c79
Thomas Zehnder <fly@zhs.ch>
2020-11-18 19:57:30 +0100

    GIT Push/pul use VSCODE

--- git_batch/git_pull.js
+++
@@ -1,3 +0,0 @@
-var shell = require("shelljs");
-
-shell.exec("git pull https://github.com/ThomasZehnder/low-noise-heating-arduino-2021.git");
--- git_batch/git_push.js
+++
@@ -1,4 +0,0 @@
-
-var shell = require("shelljs");
-
-shell.exec("git push --set-upstream https://github.com/ThomasZehnder/low-noise-heating-arduino-2021.git");
